6ea43f3b4620d09b8879ca343155b31fab8afb05
Kiril Kirilov <32368264+KirilSq@users.noreply.github.com>
2026-09-26 16:47:28 +0300

    Add: combination-target-sum - submission-2

---
+++ Data Structures & Algorithms/combination-target-sum/submission-2.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> curr = new ArrayList<>();
+        backtr(res, curr, nums, 0, target);
+        return res;
+    }
+
+    private void backtr(
+        List<List<Integer>> res, List<Integer> curr, int[] nums, int i, int target) {
+        if (target == 0) {
+            res.add(new LinkedList<>(curr));
+            return;
+        } else if (target < 0 || i >= nums.length) {
+            return;
+        } else {
+            int targetCopy = target;
+            while(targetCopy >= 0){
+              backtr(res, curr, nums, i + 1, targetCopy);
+              curr.addLast(nums[i]);
+              targetCopy-=nums[i];
+            }
+            while(targetCopy < target) {
+             curr.removeLast();
+             targetCopy+=nums[i];
+            }
+        }
+    }
+}
